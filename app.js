72e2c307f72317910dde68b5f378c5512a6b3e78
Matt Green <mgreen9@gmail.com>
2014-10-16 22:48:22 -0700

    error middleware returns json

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.send({
             message: err.message,
             error: err
         });
@@ -65,7 +65,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
         message: err.message,
         error: {}
     });
