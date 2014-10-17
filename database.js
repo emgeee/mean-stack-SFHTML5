248f8119e25ddf1ca1efd73fee019ff69a1a70ab
Matt Green <mgreen9@gmail.com>
2014-10-16 23:20:33 -0700

    add mongo URI for heroku deployment

--- database.js
+++ database.js
@@ -1,3 +1,6 @@
 
 var mongo = require('mongoskin');
-module.exports = mongo.db("mongodb://localhost:27017/sfhtml_demo", {native_parser:true});
+var uri = process.env.NODE_ENV === 'production' ? process.env.MONGOLAB_URI : "mongodb://localhost:27017/sfhtml_demo";
+
+console.log('Connecting to', uri);
+module.exports = mongo.db(uri, {native_parser:true});
