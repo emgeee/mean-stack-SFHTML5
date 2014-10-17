fedf0610351d2921bd53f3df03370486eeb13e9e
Matt Green <mgreen9@gmail.com>
2014-10-16 23:26:42 -0700

    grunt build compile to private/

--- mean_demo/Gruntfile.js
+++ mean_demo/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: '../public'
   };
 
   // Define the configuration for all the tasks
