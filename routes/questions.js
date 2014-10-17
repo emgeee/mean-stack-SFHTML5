e6802b5050335db5d3920155a0f1cf1b90f6541b
Matt Green <mgreen9@gmail.com>
2014-10-16 20:18:47 -0700

    add query params to index function

--- routes/questions.js
+++ routes/questions.js
@@ -3,14 +3,28 @@ var db = require('./../database');
 
 // get questions
 exports.index = function(req, res, next) {
-  db.collection('questions')
-    .find({})
-    .sort({votes: 1})
-    .toArray(function(err, questions) {
-      if(err) { return next(err); }
 
-      return res.send(questions);
-    });
+  var query = db.collection('questions').find({});
+
+  if(req.query.limit) {
+    query.limit(parseInt(req.query.limit,10));
+  }
+  if(req.query.offset) {
+    query.skip(parseInt(req.query.offset,10));
+  }
+
+  if(req.query.sort) {
+    var sort = req.query.sort;
+
+    if(sort === 'votes') { query.sort({votes: -1}); }
+    if(sort === 'time') { query.sort({_id: -1}); }
+  }
+
+  query.toArray(function(err, questions) {
+    if(err) { return next(err); }
+
+    return res.send(questions);
+  });
 };
 
 // create question
