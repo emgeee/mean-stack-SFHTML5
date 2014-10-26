2e3850d48f477b418b360468dd22ab72519709b1
Ward Bell <wardbell@hotmail.com>
2014-10-25 23:04:42 -0700

    renamed devQuestionsService to memQuestionsService

--- backend.v2/dataservice/index.js
+++ backend.v2/dataservice/index.js
@@ -1,3 +1,4 @@
+var serviceName;
 // Switch between the dev in-memory service and mongo service by
 // setting env var MONGO=1 or mentioning "mongo" as on of the args
 // ex (for bash/zsh): MONGO=1 npm start
@@ -6,14 +7,13 @@
 // or node-debug bin/www mongo
 var useMongoDb = process.env.MONGO ||                    // environment var
                  /~mongo/i.test(process.argv.join('~')); // launch argument
-var serviceName;
 
 if (useMongoDb) {
     console.log('Using MongoDb database');
     serviceName = './mongoQuestionsService';
 } else {
     console.log('Using in-memory database');
-    serviceName = './devQuestionsService';
+    serviceName = './memQuestionsService';
 }
 
 module.exports = require(serviceName);
