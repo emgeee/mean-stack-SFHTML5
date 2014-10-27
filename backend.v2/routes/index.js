38e9ca8ff18129f603f5226030c589e39531d0b3
Ward Bell <wardbell@hotmail.com>
2014-10-26 17:30:54 -0700

    Elevated dataservice.ready to bin/www as Matt's suggested routes/index.js no longer waits for the dataservice.

--- backend.v2/routes/index.js
+++ backend.v2/routes/index.js
@@ -1,36 +1,29 @@
-var router = require('express').Router();
-module.exports = router;
-
 var data = require('../dataservice');
-data.ready(setRoutes);
+var router = require('express').Router();
+
+module.exports = router;
 
 // router.use(notImplemented);
 
-function setRoutes(){
+/* First match wins so the order of routes really matters! */
 
-    /* First match wins so the order of routes really matters! */
+// GET /api/questions/summaries?sort={time|votes}&limit={integer}&offset={integer}
+router.get('/questions/summaries', data.getQuestionSummaries);
 
-    // GET /api/questions/summaries?sort={time|votes}&limit={integer}&offset={integer}
-    router.get('/questions/summaries', data.getQuestionSummaries);
+// GET /api/questions/:id
+router.get('/questions/:id', data.getQuestionById);
 
-    // GET /api/questions/:id
-    router.get('/questions/:id', data.getQuestionById);
+//GET /api/questions?sort={time|votes}&limit={integer}&offset={integer}
+router.get('/questions', data.getQuestions);
 
-    //GET /api/questions?sort={time|votes}&limit={integer}&offset={integer}
-    router.get('/questions', data.getQuestions);
+// POST /api/questions/:id/vote
+router.post('/questions/:id/vote', data.voteForQuestion);
 
-    // POST /api/questions/:id/vote
-    router.post('/questions/:id/vote', data.voteForQuestion);
+// POST /api/questions
+router.post('/questions', data.createQuestion);
 
-    // POST /api/questions
-    router.post('/questions', data.createQuestion);
-
-    // DELETE /api/questions/{:id
-    router.delete('/questions/:id', data.deleteQuestion);
-
-    console.log("data api routes are ready");
-}
-
+// DELETE /api/questions/{:id
+router.delete('/questions/:id', data.deleteQuestion);
 
 /////////////
 function notImplemented(req, res, next) {
